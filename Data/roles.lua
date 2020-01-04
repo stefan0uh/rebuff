ff305a9f4f78c23e5ea1b3ce58eb05660155996b
Stefan <>
2020-01-04 19:33:42 +0100

    small fix in role handling

--- Data/roles.lua
+++ Data/roles.lua
@@ -4,7 +4,7 @@ local L = LibStub("AceLocale-3.0"):GetLocale(addonName, true)
 local roles = {}
 
 -- [0] MANA | [1] RAGE | [3] ENERGY 
-table.insert(roles, {name = "tank", power = {0, 1}, classes = {"DRUID", "WARRIOR", "WARLOCK"}, modifier = {"MAINTANK"}})
+table.insert(roles, {name = "tank", power = {0, 1}, classes = {"DRUID", "WARRIOR"}})
 table.insert(roles, {name = "physical", power = {0, 1, 3}, classes = {"DRUID", "HUNTER", "ROGUE", "WARRIOR", "SHAMAN"}})
 table.insert(roles, {name = "caster", power = {0}, classes = {"DRUID", "MAGE", "PALADIN", "PRIEST", "SHAMAN", "WARLOCK"}})
 
@@ -13,19 +13,17 @@ table.insert(roles, {name = "caster", power = {0}, classes = {"DRUID", "MAGE", "
 ----------------------------
 function addon:getRole(name, modifier)
     local _, class = UnitClass(name) -- DEV CLASSNAME (English)
-    local found = nil
+    local nothing = nil
     for index, role in ipairs(roles) do
-        if modifier == nil and role.modifier == nil and addon:hasValue(role.classes, class) then -- player has no raid role and role no modifier attribute
+        if (modifier == "MAINTANK") and (role.name == "tank") and addon:hasValue(role.classes, class) then return role end
+        if addon:hasValue(role.classes, class) then
             if (role.name == "physical") then if tonumber(UnitPowerMax(name, 0)) < 4500 and addon:hasValue(role.power, UnitPowerType(name)) then return role end end
             if (role.name == "caster") then if tonumber(UnitPowerMax(name, 0)) > 4500 and addon:hasValue(role.power, UnitPowerType(name)) then return role end end
-            if addon:hasValue(role.power, UnitPowerType(name)) then return role end
-        elseif addon:hasValue(role.modifier, modifier) and addon:hasValue(role.classes, class) then
-            return role
         end
-        if addon:hasValue(role.classes, class) then found = role end
+        if (addon:hasValue(role.classes, class)) then nothing = role end
     end
-    addon:printError(name .. "|r " .. L["falseRole"] .. " (" .. found.name .. ")")
-    return found
+    addon:printError(name .. "|r " .. L["falseRole"] .. " (" .. nothing.name .. ")")
+    return nothing
 end
 
 function addon:getRoleByName(role) for k, v in pairs(roles) do if (v.name == role) then return v end end end
